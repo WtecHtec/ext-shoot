60912ab88c8d9e70ea092e078aaac9cfae837440
river <laolei@forkway.cn>
2024-03-12 00:11:24 +0800

    feat: support show cmdk

---
+++ src/component/common.tsx
@@ -0,0 +1,18 @@
+import { motion, type MotionProps } from "framer-motion"
+
+export function CMDKWrapper(
+  props: MotionProps & { children: React.ReactNode }
+) {
+  return (
+    <motion.div
+      initial={{ opacity: 0, scale: 0.98 }}
+      animate={{ opacity: 1, scale: 1 }}
+      exit={{ opacity: 0, scale: 0.98 }}
+      transition={{ duration: 0.2 }}
+      style={{
+        height: 475
+      }}
+      {...props}
+    />
+  )
+}
--- src/content.tsx
+++ src/content.tsx
@@ -1,22 +1,27 @@
+import componentStyles from "data-text:~style.all.scss"
 import cssText from "data-text:~style.css"
 import type { PlasmoCSConfig } from "plasmo"
 
-import { CountButton } from "~component/count-button"
-import SwitchDemo from "~component/switch/switch"
+import { RaycastCMDK } from "~component/cmdk/menu"
+import { CMDKWrapper } from "~component/common"
+
 export const config: PlasmoCSConfig = {
   matches: ["<all_urls>"]
 }
 
 export const getStyle = () => {
   const style = document.createElement("style")
-  style.textContent = cssText
+  style.textContent = cssText + componentStyles
   return style
 }
 
 const PlasmoOverlay = () => {
   return (
     <div className="fixed z-50 flex items-center justify-center transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2">
-      <SwitchDemo />
+      
+      <CMDKWrapper>
+        <RaycastCMDK />
+      </CMDKWrapper>
     </div>
   )
 }

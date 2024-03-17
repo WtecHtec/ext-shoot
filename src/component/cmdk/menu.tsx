db2bdcff6e767fa40c6346ea2af82a7cfccffe78
river <laolei@forkway.cn>
2024-03-17 11:12:58 +0800

    chore: update style

--- src/component/cmdk/menu.tsx
+++ src/component/cmdk/menu.tsx
@@ -61,10 +61,10 @@ export function RaycastCMDK() {
 	 * 		name: 'Development'
 	 *  }
 	 *  {
-	 *   name: 'All'  
+	 *   name: 'All'
 	 *  }
 	 * ]
-	 * @returns 
+	 * @returns
 	 */
 	const getExtensionDatas = async () => {
 		const [err, res] = await getExtensionAll()
@@ -324,7 +324,7 @@ function SubCommand({
 
 	React.useEffect(() => {
 		function listener(e: KeyboardEvent) {
-			if (e.key === "K" && e.metaKey) {
+			if (e.key === "k" && e.metaKey) {
 				e.preventDefault()
 				setOpen((o) => !o)
 			}

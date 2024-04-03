ad029976c2592e64a117010f3d01018a4c884631
river <laolei@forkway.cn>
2024-04-03 10:29:45 +0800

    feat: update mode value in config.ts

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,3 +1,4 @@
-export const mode: 'dev' | 'pro' = 'dev'; // 这里指定 mode 可以是 'dev' 或 'pro'
+export const mode: 'dev' | 'pro' =
+    process.env.PLASMO_PUBLIC_MODE === 'dev' ? 'dev' : 'pro';
 
 export const SearchFix = 'search_';

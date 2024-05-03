92aeb7208f62a4634256303dabd11f0d346a8b88
river <laolei@forkway.cn>
2024-05-03 15:14:39 +0800

    Refactor component imports and add new extensions

--- src/component/cmdk/search-group.tsx
+++ src/component/cmdk/search-group.tsx
@@ -2,11 +2,11 @@
 
 import React from 'react';
 import { Command } from 'cmdk';
-import ChromeStoreWebSearch from '~component/extension/chrome-store-web-search';
-import InstantOpen from '~component/extension/instant-open';
-import ChromeStoreSearch from '~component/extension/chrome-store-search';
+import ChromeStoreWebSearch from '~extension/chrome-store-web-search';
+import InstantOpen from '~extension/instant-open';
+import ChromeStoreSearch from '~extension/chrome-store-search';
 const Search = ({ search }: { search: string}) => {
-	
+
 	return <Command.Group heading={`Use "${search}" with ... `}>
 		<ChromeStoreSearch search={search} ></ChromeStoreSearch>
 		<ChromeStoreWebSearch search={search} ></ChromeStoreWebSearch>

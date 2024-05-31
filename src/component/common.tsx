d64be2d29498551ce5a9cd2508c819bcb1c5ea81
river <laolei@forkway.cn>
2024-05-31 22:06:39 +0800

    refactor: Update CMDKWrapper initial scale value to 1

--- src/component/common.tsx
+++ src/component/common.tsx
@@ -4,7 +4,7 @@ import React from 'react';
 export function CMDKWrapper(props: MotionProps & { children: React.ReactNode }) {
   return (
     <motion.div
-      initial={{ opacity: 0, scale: 0.96 }}
+      initial={{ opacity: 0, scale: 1 }}
       animate={{ opacity: 1, scale: 1 }}
       exit={{ opacity: 0, scale: 0.98 }}
       transition={{ duration: 0.2 }}
